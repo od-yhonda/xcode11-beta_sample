1ffd3f8ec413134aab0842341e477545b2b24cea
yhonda <yhonda@opendoor.co.jp>
2019-08-30 11:01:49 +0900

    Initial Commit

---
+++ xcode11-beta_sampleTests/xcode11_beta_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  xcode11_beta_sampleTests.swift
+//  xcode11-beta_sampleTests
+//
+//  Created by yhonda on 2019/08/30.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import XCTest
+@testable import xcode11_beta_sample
+
+class xcode11_beta_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
